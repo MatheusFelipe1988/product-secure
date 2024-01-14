79dcf5958b85301cbf254cac01305e7bff2ac736
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-01-14 17:20:02 -0400

    Update Preliminar
    Authenticação e autorização para a tabela usuário concluida

---
+++ security/src/main/java/com/system/security/configuration/CustomBasicAuthenticationFilter.java
@@ -0,0 +1,94 @@
+package com.system.security.configuration;
+
+import com.system.security.domain.User;
+import com.system.security.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+@RequiredArgsConstructor
+public class CustomBasicAuthenticationFilter extends OncePerRequestFilter {
+    private final UserRepository repository;
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BASIC = "Basic";
+
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (isBasicAunthentication(request)){
+            String[] credentials = decodeBase64(getHeader(request)
+                    .replace(BASIC, ""))
+                    .split(":");
+
+            String username = credentials[0];
+            String password = credentials[1];
+
+            User user = repository.findByUsername(username);
+
+            if(user == null){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("No exist this user");
+
+                return;
+            }
+            boolean valid = checkPassword(user.getPassword(), password);
+                if(!valid) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Password not match");
+                    return;
+
+                }
+                setAuthentication(user);
+
+            }
+        filterChain.doFilter(request, response);
+
+        }
+
+    private void setAuthentication(User user) {
+        Authentication authentication = createAuthenticationToken(user);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private Authentication createAuthenticationToken(User user) {
+        UserPrincipal userPrincipal = UserPrincipal.create(user);
+        return new UsernamePasswordAuthenticationToken
+                (userPrincipal, null, userPrincipal.getAuthorities());
+    }
+
+    private boolean checkPassword(String userPassword, String loginPassword1) {
+        return passwordEncoder().matches(loginPassword1, userPassword);
+    }
+
+    private String decodeBase64(String base64) {
+        byte[] decodeBytes = Base64.getDecoder().decode(base64);
+        return new String(decodeBytes);
+    }
+
+    private boolean isBasicAunthentication(HttpServletRequest request) {
+        String header = getHeader(request);
+        return header != null && header.startsWith(BASIC);
+    }
+    private String getHeader(HttpServletRequest request){
+        return request.getHeader(AUTHORIZATION);
+    }
+}
